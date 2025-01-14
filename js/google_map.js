a7b096aa0e1cf4e1da5a40ee9278abe81c3b723b
Samuel Rigaux <samuel.rigaux@laplateforme.io>
2025-01-14 12:09:38 +0100

    Addition of the contact section and finalization of the portfolio.

--- js/google_map.js
+++ js/google_map.js
@@ -5,7 +5,7 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var myLatlng = new google.maps.LatLng(43.30231137716497, 5.370200867547211);
     // 39.399872
     // -8.224454
     
